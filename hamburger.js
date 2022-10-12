7774602caf709d01cf450fce25261662fe1f5e60
ArkanKovanAbdullah <79310452+ArkanKovanAbdullah@users.noreply.github.com>
2022-10-12 03:41:49 +0300

    Linter errors fixed

--- hamburger.js
+++ hamburger.js
@@ -1,28 +1,27 @@
 const navigationLinksMobile = document.querySelectorAll('.mobile-navs');
 const menuBtn = document.querySelector('.btn-menu');
-const mobileeNav = document.querySelector('.mobile-navigation')
-const hba = document.querySelector('.hamburger')
+const mobileeNav = document.querySelector('.mobile-navigation');
+const hba = document.querySelector('.hamburger');
 
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
-  if(!menuOpen) {
+  if (!menuOpen) {
     menuBtn.classList.add('open');
     mobileeNav.classList.remove('display-none');
-    hba.classList.add('chnage-color')
+    hba.classList.add('chnage-color');
     navigationLinksMobile.forEach((item) => {
       item.addEventListener('click', () => {
         mobileeNav.classList.add('display-none');
         menuBtn.classList.remove('open');
-        hba.classList.remove('chnage-color')
+        hba.classList.remove('chnage-color');
         menuOpen = false;
-        });
+      });
     });
     menuOpen = true;
   } else if (menuOpen) {
     menuBtn.classList.remove('open');
-    mobileeNav.classList.add('display-none')
-    hba.classList.remove('chnage-color')
+    mobileeNav.classList.add('display-none');
+    hba.classList.remove('chnage-color');
     menuOpen = false;
   }
 });
-
